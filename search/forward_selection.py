4aaf9856b46788034be5dbb22453eb5806993320
williamEkim <dongyeon0308@gmail.com>
2025-12-07 15:06:33 -0800

    fixed the logic(pretty much reverted back)

--- search/forward_selection.py
+++ search/forward_selection.py
@@ -8,9 +8,9 @@ def forward_selection(num_features, dataset):
     ans_set = [] 
     best_set = [] 
     best_acc = 0
-    improved = False
     validator = Validator()
     classifier = NNClassifier()
+
 
     print("Using 0 features, using “leaving-one-out” evaluation, accuracy is ", round(best_acc,2), "%")
     print("Beginning Search.")
@@ -18,42 +18,41 @@ def forward_selection(num_features, dataset):
 
     #iterate throught lvls 
     for level in range(1,num_features + 1):  
-        feature_to_add = None
-        best_acc_so_far = -1 
+        feature_to_add = None 
+        best_acc_so_far = -1
 
         #try adding each feature that's not alr in the set
         for feature in range(num_features): 
             if feature not in ans_set: 
-                curr_set = ans_set[:] 
+                curr_set = ans_set[:]
                 curr_set.append(feature) 
 
                 accuracy = validator.evaluate(dataset, classifier, curr_set)
 
                 print("    Using feature(s) ", [ f+1 for f in curr_set ], " accuracy is ", round(accuracy*100,2), "%")
 
-                #check to see if there is a better accuracy 
-                if accuracy > best_acc_so_far: 
+                #check to see if there is a better accuracy overall
+                if accuracy > best_acc_so_far:
                     best_acc_so_far = accuracy
                     feature_to_add = feature
 
-
         print("")
-        if best_acc_so_far <= best_acc: 
-            print("Warning!!! Accuracy has decreased, continuing search in case of local maxima...")
-
-        # if there is no possible improvement, do not update best set
-        else:
-            #add the best feature on this lvl (Lvls are [1], [1,2], [1.2.3], etc)
+        if feature_to_add is not None:
             ans_set.append(feature_to_add)
-            best_acc = best_acc_so_far
-            best_set = ans_set[:]
+
+            # update global best if local best beats it
+            if best_acc_so_far > best_acc:
+                best_acc = best_acc_so_far
+                best_set = ans_set[:]
+            else:
+                print("Warning!!! Accuracy has decreased or stayed same, continuing search in case of local maxima...")
+
 
         print("Feature set ", [ f+1 for f in ans_set ], " was the best with an accuracy of ", round(best_acc_so_far*100,2), "%\n")
 
 
-            
+    print("Finished Forward Selection Search!")
 
-    print("Finished Search!! The best feature subset is ", [ f+1 for f in best_set ], "with an accuracy of ", round(best_acc*100,2), "%")
     return {
         "best_set": best_set, 
         "best_acc": best_acc
